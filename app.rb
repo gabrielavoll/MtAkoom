19e5008a42ffae458374ab9305fc707923fabb1e
Gabriela Voll <gabriela.voll@youvisit.com>
2016-09-13 01:45:26 -0400

    successfully embeded game

--- app.rb
+++ app.rb
@@ -17,3 +17,11 @@ get '/stylesheets/:name.css' do
   content_type 'text/css', :charset => 'utf-8'
   scss(:"stylesheets/#{params[:name]}", Compass.sass_engine_options )
 end
+
+get '/Mt.AkoomWeb.unity3d' do
+	File.read('Mt.AkoomWeb.unity3d')
+end
+
+get '/UnityObject2.js' do
+	File.read('UnityObject2.js')
+end
